3e5429ffc4c059bebd2582e1e82ffc2128cbbe97
vbouchet <vincent.bouchet.31@gmail.com>
2015-08-27 16:14:36 +0200

    Rename SlideshowSkin to ViewsSlideshowSink

---
+++ src/Plugin/ViewsSlideshowSkin/DefaultSkin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\views_slideshow\Plugin\ViewsSlideshowSkin\DefaultSkin.
+ */
+
+namespace Drupal\views_slidehow\Plugin\ViewsSlideshowSkin;
+
+use Drupal\views_slideshow\ViewsSlideshowSkinBase;
+
+/**
+ * Class DefaultSkin
+ * @package Drupal\views_slideshow\Plugin\ViewsSlideshowSkin
+ *
+ * @ViewsSlideshowSkin(
+ *   id = "default",
+ *   label = @Translation("Default")
+ * )
+ */
+class DefaultSkin extends ViewsSlideshowSkinBase {}
